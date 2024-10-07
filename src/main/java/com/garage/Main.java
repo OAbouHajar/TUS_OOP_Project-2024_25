48b5db249d58d856528fadc527edcf8e54430ae5
Osama Abou Hajar <smsm.sy@hotmail.com>
2024-10-07 17:42:49 +0100

    skilaton maven project

---
+++ src/main/java/com/garage/Main.java
@@ -0,0 +1,18 @@
+/*
+*
+* Owner: Osame Abou Hajar
+* Student ID: A00334533
+* OOP1 project 24/25
+*
+* */
+
+
+
+package com.garage;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
